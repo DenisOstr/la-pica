c4a35f9518768d4c5ef1e43528920aafa9834b2e
Denis Ostrovsky <denisostrovskis1@gmail.com>
2023-08-10 17:55:56 +0300

    add REST resources for Pizza model

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { PizzasModule } from './pizzas/pizzas.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, PizzasModule],
   controllers: [AppController],
   providers: [AppService],
 })
