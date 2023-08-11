4f0c3cc0f338d3693b68a67023d0fe23f503af50
Denis Ostrovsky <denisostrovskis1@gmail.com>
2023-08-12 01:46:40 +0300

    update ui components

--- frontend/src/components/UI/BaseButton.vue
+++ frontend/src/components/UI/BaseButton.vue
@@ -1,10 +1,13 @@
 <template>
     <button
-        class="flex items-center text-sm font-medium rounded-md"
+        class="text-sm font-medium rounded-md"
         :class="{
+            'flex items-center': color != 'ternary',
             'space-x-1': size != 'icon',
             'bg-black text-white hover:bg-opacity-80': color == 'primary',
             'border border-gray-300 hover:bg-gray-100': color == 'secondary',
+            'text-gray-500 transition-colors duration-200 rounded-lg hover:bg-gray-200': color == 'ternary',
+            'bg-red-500 text-white hover:bg-opacity-80': color == 'error',
             'px-3 py-2': size == 'sm', 'px-3 py-1': size == 'xs', 'p-1': size == 'icon'
         }"
     >
--- frontend/src/components/UI/BaseTable.vue
+++ frontend/src/components/UI/BaseTable.vue
@@ -11,7 +11,7 @@
                 </tr>
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
-                <tr class="hover:bg-gray-100 hover:cursor-pointer" v-for="item in data">
+                <tr class="hover:bg-gray-100 hover:cursor-pointer" v-for="item in data" @click="$emit('openItem', item.id)">
                     <td class="px-4 py-4 text-sm whitespace-nowrap">
                         <div>
                             <h4 class="text-gray-700">{{ item.name }}</h4>
@@ -20,7 +20,7 @@
 
                     <td class="px-4 py-4 text-sm whitespace-nowrap">
                         <div>
-                            <h4 class="text-gray-700">{{ item.price }}</h4>
+                            <h4 class="text-gray-700">{{ item.price }}€</h4>
                         </div>
                     </td>
 
@@ -31,21 +31,38 @@
                     </td>
 
                     <td class="px-4 py-4 text-sm text-right whitespace-nowrap">
-                        <button class="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg hover:bg-gray-200">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
-                            </svg>
-                        </button>
+                        <BaseButton color="ternary" size="icon" icon="fluent:more-vertical-20-regular" @click="isOptionsOpened = !isOptionsOpened" />
+
+                        <div class="text-left absolute right-12 top-[300px] bg-white w-40 min-w-40 p-1 shadow-md rounded-md" v-if="isOptionsOpened">
+                            <div class="flex items-center p-2 hover:bg-gray-200 rounded-md space-x-2">
+                                <Icon icon="iconoir:edit-pencil" class="text-base" />
+                                <p>Edit</p>
+                            </div>
+                            
+                            <div class="flex items-center p-2 text-red-500 hover:bg-gray-200 rounded-md space-x-2">
+                                <Icon icon="iconoir:trash" class="text-base" />
+                                <p>Delete</p>
+                            </div>
+                        </div>
                     </td>
                 </tr>
             </tbody>
         </table>
+
+        <div class="text-center font-semibold p-2" v-if="data.length == 0">Data not found</div>
     </div>
 </template>
 
 <script setup lang="ts">
+    import { ref } from 'vue'
+    import { Icon } from '@iconify/vue'
+
+    import BaseButton from './BaseButton.vue'
+
     defineProps<{
         columns: string[],
         data: any
     }>()
+
+    const isOptionsOpened = ref(false)
 </script>
