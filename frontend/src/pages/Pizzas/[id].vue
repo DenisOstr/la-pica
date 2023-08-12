8965ddb920c1ee9a97adbb264f9d35fd7ccbd7e3
Denis Ostrovsky <denisostrovskis1@gmail.com>
2023-08-12 16:36:39 +0300

    add update and delete pizza

--- frontend/src/pages/Pizzas/[id].vue
+++ frontend/src/pages/Pizzas/[id].vue
@@ -2,13 +2,13 @@
     <div class="space-y-6">
         <div class="flex items-center justify-between">
             <div>
-                <h1 class="text-xl font-semibold">{{ pizza?.name }} <span class="text-sm text-green-500">({{ pizza?.price }}€)</span></h1>
+                <h1 class="text-xl font-semibold">{{ pizza?.name }} <span class="text-sm text-green-500">({{ pizza?.price.toFixed(2) }}€)</span></h1>
                 <p class="text-sm text-gray-500">Update or delete pizza</p>
             </div>
 
             <div class="flex space-x-2">
-                <BaseButton @click="() => {}">Save</BaseButton>
-                <BaseButton color="error" @click="() => {}">Delete</BaseButton>
+                <BaseButton @click="updatePizza">Update</BaseButton>
+                <BaseButton color="error" @click="deletePizza">Delete</BaseButton>
             </div>
         </div>
 
@@ -25,7 +25,11 @@
                         <Icon icon="fluent:re-order-dots-vertical-16-regular" class="cursor-pointer" />
                         <p class="text-sm font-semibold">{{ ingredient.name }}</p>
                     </div>
-                    <p class="text-sm font-medium">{{ ingredient.price }}€</p>
+
+                    <div class="flex items-center space-x-2">
+                        <p class="text-sm font-medium">{{ ingredient.price.toFixed(2) }}€</p>
+                        <Icon icon="iconoir:cancel" class="hover:text-red-500 cursor-pointer" @click="deleteIngredient(ingredient)" />
+                    </div>
                 </div>
             </div>
 
@@ -47,7 +51,7 @@
         <div class="p-4 space-y-4">
             <p class="text-sm text-red-500" v-if="showExistError">Ingredient already added to the pizza</p>
             
-            <div class="bg-gray-100 w-full p-2 max-h-52 rounded-md overflow-auto" v-if="allIngredients.length != 0">
+            <div class="bg-gray-100 w-full p-2 max-h-52 rounded-md overflow-auto space-y-1" v-if="allIngredients.length != 0">
                 <div
                     class="flex justify-between p-1 rounded cursor-pointer"
                     :class="{
@@ -55,10 +59,10 @@
                         'hover:bg-gray-300 hover:text-gray-800': !ingredientsList.includes(ingredient)
                     }"
                     v-for="ingredient in allIngredients"
-                    @click="ingredientsList.push(ingredient)"
+                    @click="addIngredientToList(ingredient)"
                 >
                     <p class="text-sm font-medium">{{ ingredient.name }}</p>
-                    <p class="text-sm">{{ ingredient.price }}€</p>
+                    <p class="text-sm">{{ ingredient.price.toFixed(2) }}€</p>
                 </div>
             </div>
 
@@ -77,7 +81,7 @@
 </template>
 
 <script setup lang="ts">
-    import { ref, computed, watchEffect } from 'vue'
+    import { ref, computed, watchEffect, onMounted } from 'vue'
     import { Icon } from '@iconify/vue'
     import { useRouter } from 'vue-router'
     import { useQuery, useMutation } from '@/composables/useAPI'
@@ -96,7 +100,6 @@
     const name = ref('')
     const ingredients = ref<any>({})
     const ingModalOpened = ref(false)
-    const pizzaIngredients = ref<any>([])
     const ingredientsList = ref<any>([])
     const inputFieldShowed = ref(false)
     const ingredientName = ref('')
@@ -110,14 +113,16 @@
 
     const result = useQuery(`http://localhost:3000/pizzas/${pizzaId.value}`)
     
-    watchEffect(async () => {
+    onMounted(async () => {
         result.then((res) => {
             pizza.value = res.data.pizzas
 
             name.value = pizza.value.name
             ingredients.value = JSON.parse(pizza.value.ingredients)
         })
+    })
 
+    watchEffect(async () => {
         if (ingModalOpened.value) {
             const result = await useQuery('http://localhost:3000/ingredients')
 
@@ -145,11 +150,31 @@
         }
     }
 
+    function addIngredientToList(item: any) {
+        const ingredientExist = ingredientsList.value.some((ingredient: any) => {
+            return ingredient.name === item.name
+        })
+
+        if (ingredientExist) {
+            const ingredientItem = ingredientsList.value.findIndex((ingredient: any) => {
+                return ingredient.name == item.name
+            })
+
+            ingredientsList.value.splice(ingredientItem, 1)
+        } else {
+            ingredientsList.value.push(item)
+        }
+    }
+
     function applyIngredients() {
         if (ingredientsList.value.length != 0) {
-            for (const ingredient in ingredientsList.value) {
-                if (pizzaIngredients.value.indexOf(ingredientsList.value[ingredient]) == -1) {
-                    pizzaIngredients.value.push(ingredientsList.value[ingredient])
+            for (const ingredientItem in ingredientsList.value) {
+                const ingredientExist = ingredients.value.some((ingredient: any) => {
+                    return ingredient.name === ingredientsList.value[ingredientItem].name
+                })
+
+                if (!ingredientExist) {
+                    ingredients.value.push(ingredientsList.value[ingredientItem])
                 } else {
                     showExistError.value = true
                     return
@@ -157,6 +182,31 @@
             }
             
             ingModalOpened.value = false
+        }
+    }
+
+    function deleteIngredient(ingredient: any) {
+        const ingredientItem = ingredients.value.findIndex((item: any) => {
+            return item.name == ingredient.name
+        })
+
+        ingredients.value.splice(ingredientItem, 1)
+    }
+
+    async function deletePizza() {
+        await useMutation(`http://localhost:3000/pizzas/${pizzaId.value}`, 'delete', null)
+
+        router.push('/pizzas')
+    }
+
+    async function updatePizza() {
+        if (name.value != '' && ingredients.value.length != 0) {
+            await useMutation(`http://localhost:3000/pizzas/${pizzaId.value}`, 'patch', {
+                name: name.value,
+                ingredients: ingredients.value
+            })
+
+            router.push('/pizzas')
         }
     }
 </script>
