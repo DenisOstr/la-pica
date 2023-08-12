23104e5a833f2ba8b352af6c88296228371c7443
Denis Ostrovsky <denisostrovskis1@gmail.com>
2023-08-12 17:58:17 +0300

    add illustration for overview page

--- frontend/src/pages/Overview.vue
+++ frontend/src/pages/Overview.vue
@@ -1,5 +1,14 @@
 <template>
     <div class="space-y-6">
         <h1 class="text-xl font-semibold">Overview</h1>
+
+        <div class="flex flex-col items-center">
+            <img src="@/assets/illustration.svg" alt="" class="w-2/6">
+            
+            <div class="text-center">
+                <h1 class="text-2xl font-bold">Welcome To La Pica</h1>
+                <p class="text-sm text-gray-400">Create and manage pizzas and ingredients</p>
+            </div>
+        </div>
     </div>
 </template>
